823302a738141ec388a974ac492211dc057fce54
Yash Shah <yashshah518@gmail.com>
2021-04-07 19:35:52 +0530

    added flask server

---
+++ app.py
@@ -0,0 +1,173 @@
+import numpy as np
+from PIL import Image
+from numpy import array
+from keras.preprocessing import image
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import Model
+from keras.applications.inception_v3 import preprocess_input
+from keras.models import load_model
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from pickle import dump, load
+from flask import Flask,jsonify,request,render_template
+import os
+
+app=Flask(__name__)
+app.config['UPLOAD_FOLDER']='images'
+
+trained_model=load_model("trained_model.h5")
+model = InceptionV3(weights='imagenet')
+# Create a new model, by removing the last layer (output layer) from the inception v3
+model_new = Model(model.input, model.layers[-2].output)
+
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+    # process line by line
+    for line in doc.split('\n'):
+        # skip empty lines
+        if len(line) < 1:
+            continue
+        # get the image identifier
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+# load training dataset (6K)
+filename = 'train.txt'
+train = load_set(filename)
+
+# load clean descriptions into memory
+def load_clean_descriptions(filename, dataset):
+    # load document
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        # split line by white space
+        tokens = line.split()
+        # split id from description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # skip images not in the set
+        if image_id in dataset:
+            # create list
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+            # wrap description in tokens
+            desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+            # store
+            descriptions[image_id].append(desc)
+    return descriptions
+
+# descriptions
+train_descriptions = load_clean_descriptions('descriptions.txt', train)
+print('Descriptions: train=%d' % len(train_descriptions))
+
+all_train_captions = []
+for key, val in train_descriptions.items():
+    for cap in val:
+        all_train_captions.append(cap)
+
+# Consider only words which occur at least 10 times in the corpus
+word_count_threshold = 10
+word_counts = {}
+nsents = 0
+for sent in all_train_captions:
+    nsents += 1
+    for w in sent.split(' '):
+        word_counts[w] = word_counts.get(w, 0) + 1
+
+vocab = [w for w in word_counts if word_counts[w] >= word_count_threshold]
+print('preprocessed words %d -> %d' % (len(word_counts), len(vocab)))
+
+ixtoword = {}
+wordtoix = {}
+
+ix = 1
+for w in vocab:
+    wordtoix[w] = ix
+    ixtoword[ix] = w
+    ix += 1
+
+# convert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+
+# calculate the length of the description with the most words
+def max_length(descriptions):
+    lines = to_lines(descriptions)
+    return max(len(d.split()) for d in lines)
+
+# determine the maximum sequence length
+max_length = max_length(train_descriptions)
+
+def preprocess(image_path):
+    # Convert all the images to size 299x299 as expected by the inception v3 model
+    img = image.load_img(image_path, target_size=(299, 299))
+    # Convert PIL image to numpy array of 3-dimensions
+    x = image.img_to_array(img)
+    # Add one more dimension
+    x = np.expand_dims(x, axis=0)
+    # preprocess the images using preprocess_input() from inception module
+    x = preprocess_input(x)
+    return x
+
+# Function to encode a given image into a vector of size (2048, )
+def encode(image):
+    image = preprocess(image) # preprocess the image
+    fea_vec = model_new.predict(image) # Get the encoding vector for the image
+    fea_vec = np.reshape(fea_vec, fea_vec.shape) # reshape from (1, 2048) to (2048, )
+    return fea_vec
+
+#generate caption iteratively
+def greedySearch(photo):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = [wordtoix[w] for w in in_text.split() if w in wordtoix]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = trained_model.predict([photo,sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = ixtoword[yhat]
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    final = in_text.split()
+    final = final[1:-1]
+    final = ' '.join(final)
+    return final
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/generate-caption",methods=["Post"])
+def caption_img():
+    im=request.files['file']
+    im.save(os.path.join(app.config['UPLOAD_FOLDER'], im.filename))
+    test_image = image.load_img('images/'+im.filename)
+    encoded_image=encode('images/'+im.filename)
+    #encoded_image.reshape((1,2048))
+    caption=greedySearch(encoded_image)
+    return caption
+
+
+
+
+if __name__== "__main__":
+    app.run(debug=True)
